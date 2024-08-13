06b83521cf7196c03351503898a7e0b5486382f2
Nikita <nesqui@bk.ru>
2024-08-13 18:34:43 +0300

    feat: component style, render function

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
-  <div class="p-5">
-    <div class="mb-2">
-      <p>Инпуты связаны переменной, компонент позволяет редактирование значения и не падает если в нем появятся символы
-      </p>
-      <p>Переменная: {{ number }}</p>
-    </div>
-    <div class="mb-1">
-      <label for="basic-input">Обычный input:</label>
-      <input type="text" id="basic-input" v-model="number">
-    </div>
-    <label for="basic-input">Кастомный input:</label>
-    <NumericInput v-model="number" />
+  <div class="app">
+    <NumericInput imgSrc="./photo.png" label="my label" caption="my caption" v-model="number" />
   </div>
 </template>
 
@@ -21,11 +11,10 @@ const number = ref()
 </script>
 
 <style scoped>
-input {
-  margin-bottom: 1rem;
-}
-
-label {
-  display: block;
+.app {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- src/components/NumericInput.vue
+++ src/components/NumericInput.vue
@@ -1,25 +1,63 @@
 <template>
-    <input class="resizable-input" @beforeinput="(e: Event) => onBeforeinput(e as InputEvent)" @paste="onPaste"
-        :value="number" @input="onInputEventHandler" name="number-nikita" id="number-nikita" />
+    <div :class="{ active: focus }" class="numeric-input">
+        <div class="numeric-input__image__wrapper">
+            <img v-if="imgSrc" :src="imgSrc" alt="" class="numeric-input__image">
+        </div>
+        <div class="numeric-input__wrapper">
+            <label for="number-nikita" class="numeric-input__label">{{ label }}</label>
+            <div class="numeric-input__caption">
+                <input ref="inputRef" class="numeric-input__input" @focusin="onFocusin" @focusout="onFocusout"
+                    @beforeinput="(e: Event) => onBeforeinput(e as InputEvent)" @paste="onPaste" :value="number"
+                    @input="onInputEventHandler" name="number-nikita" id="number-nikita" />
+                {{ caption }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const props = defineProps({
     modelValue: {
         required: false,
         type: String
+    },
+    imgSrc: {
+        required: false,
+        type: String
+    },
+    label: {
+        required: true,
+        type: String,
+        default: 'label'
+    },
+    caption: {
+        required: true,
+        type: String,
+        default: 'caption'
     }
 })
 
 const emit = defineEmits(['update:modelValue'])
 const number = ref()
+const focus = ref()
+const inputRef = ref()
+const paddingLeft = ref('8px')
+const paddingRight = ref('16px')
 
 const regexDigits = /^[0-9\s]+$/;
 
 const width = ref('72px')
 const minWidth = ref('72px')
+
+const onFocusin = () => {
+    focus.value = true
+}
+
+const onFocusout = () => {
+    focus.value = false
+}
 
 const onPaste = (e: ClipboardEvent) => {
     e.preventDefault()
@@ -32,6 +70,36 @@ const onBeforeinput = (e: InputEvent) => {
         return
     }
 }
+
+const tempSpan = document.createElement('span');
+
+const resizeInput = () => {
+    const input = inputRef.value;
+    const style = getComputedStyle(input);
+    tempSpan.style.fontFamily = style.fontFamily;
+    tempSpan.style.fontSize = style.fontSize;
+    tempSpan.style.fontWeight = style.fontWeight;
+    tempSpan.style.fontStyle = style.fontStyle;
+    tempSpan.style.letterSpacing = style.letterSpacing;
+    tempSpan.textContent = '' + number.value;
+    document.body.appendChild(tempSpan);
+    width.value = `calc(${paddingLeft.value} + ${paddingRight.value} + ${tempSpan.offsetWidth + 2 + 'px'})`;
+    document.body.removeChild(tempSpan);
+}
+
+function debounce<T extends unknown[]>(fn: (...args: T) => void, delay: number): (...args: T) => void {
+    let timeoutId: number | undefined;
+
+    return (...args: T) => {
+        if (timeoutId !== undefined) clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+}
+
+const debouncedResizeInput = debounce(resizeInput, 15)
 
 const onInputHandler = (value: string) => {
     let str = []
@@ -57,7 +125,8 @@ const onInputHandler = (value: string) => {
     }
 
     emit('update:modelValue', number.value)
-    width.value = `${number.value.length}ch`
+    debouncedResizeInput()
+
 }
 
 const onInputEventHandler = (e: Event) => {
@@ -76,18 +145,69 @@ watch(() => props.modelValue, () => {
 </script>
 
 <style scoped>
-.resizable-input {
-    width: v-bind(width);
-    min-width: v-bind(minWidth);
+.numeric-input {
+    display: flex;
+    gap: 16px;
 }
 
-.resizable-input::-webkit-outer-spin-button,
-.resizable-input::-webkit-inner-spin-button {
+.active .numeric-input__image__wrapper {
+    outline: 1px solid var(--primary, #3D06D7);
+}
+
+.active .numeric-input__label {
+    color: var(--primary, #3D06D7)
+}
+
+.active .numeric-input__input {
+    outline: 1.5px solid var(--primary-light, #906FEE);
+    color: var(--dark, #1E0E4C);
+}
+
+.numeric-input__label {
+    font-size: 16px;
+    line-height: 15px;
+    letter-spacing: 0.32px;
+    font-family: Koulen;
+    color: var(--dark, #1E0E4C);
+}
+
+.numeric-input__image__wrapper {
+    padding: 4px;
+    border-radius: 50%;
+}
+
+.numeric-input__image {
+    height: 80px;
+    width: 80px;
+}
+
+.numeric-input__caption {
+    color: var(--dark, #1E0E4C);
+    display: flex;
+    gap: 12px;
+    align-items: center;
+}
+
+.numeric-input__input {
+    font-family: "Inter", sans-serif;
+    font-size: 1em;
+    width: v-bind(width);
+    color: var(--light-grey, #CFCADF);
+    min-width: v-bind(minWidth);
+    padding: 8px v-bind(paddingRight) 8px v-bind(paddingLeft);
+    border-radius: 6px;
+    height: 44px;
+    outline: 1px solid var(--light-grey, #CFCADF);
+    transition: all 0.5s ease;
+}
+
+.numeric-input__input::-webkit-outer-spin-button,
+.numeric-input__input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
 }
 
-.resizable-input[type=number] {
+.numeric-input__input[type=number] {
     -moz-appearance: textfield;
 }
 </style>
